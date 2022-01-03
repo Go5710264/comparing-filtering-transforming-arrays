2f24c99e7f0718dbfb5d5b35cb66714276fc2e44
Dima Shirkov <dimashirkov@list.ru>
2022-01-03 21:48:41 +0300

    Dak1 draft

---
+++ task.js
@@ -0,0 +1,16 @@
+function compareArrays(arr1, arr2) {
+  
+  let result = arr1.length === arr2.length && arr1.every(function(currentValue, index) { 
+    return currentValue === arr2[index];
+  })
+  
+  return result; // boolean
+}
+
+function advancedFilter(arr) {
+  let resultArr;
+
+  // Ваш код
+
+  return resultArr; // array
+}
