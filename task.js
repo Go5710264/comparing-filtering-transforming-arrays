d05a9aa5c6207357582864a128425982383000c0
Dima Shirkov <dimashirkov@list.ru>
2023-04-07 14:20:52 +0300

    corrected README.md, renamed JS file, removed comments

--- task.js
+++
@@ -1,7 +0,0 @@
-function compareArrays(arr1, arr2) { 
-  return arr1.length === arr2.length && arr1.every((currentValue, index) => currentValue === arr2[index]);  
-}
-
-function advancedFilter(arr) {
-  return resultArr = arr.filter((selectPosivive) => selectPosivive >= 1).filter((cardinalityCheck) => cardinalityCheck % 3 === 0).map((multiplication) => multiplication * 10);  
-}
